63355d6219e37e9d5626a4cc45148c25be45785a
rifatsaown <rifatsaown0@gmail.com>
2022-09-29 22:27:20 +0600

    tuple data type

---
+++ dist/tuple-type.js
@@ -0,0 +1,7 @@
+"use strict";
+// mixed data - key,value
+let user; // tuple data type
+user = [101, "John"];
+user.push(102, "Jane");
+console.log(user);
+console.log(user[0]);
---
+++ src/tuple-type.ts
@@ -0,0 +1,6 @@
+// mixed data - key,value
+let user:[number,string]; // tuple data type
+user = [101,"John"];
+user.push(102,"Jane");
+console.log(user);
+console.log(user[0]);
