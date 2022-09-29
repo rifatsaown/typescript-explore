3e9414731ac1884b027e0561656813fd1f42c980
rifatsaown <rifatsaown0@gmail.com>
2022-09-29 22:41:16 +0600

    any data type

---
+++ dist/any-type.js
@@ -0,0 +1,7 @@
+"use strict";
+let anyName; // any type is used to store any type of data
+anyName = 10;
+anyName = "Hello";
+anyName = true;
+anyName = { name: "John" };
+anyName = [1, 2, 3];
---
+++ src/any-type.ts
@@ -0,0 +1,6 @@
+let anyName: any; // any type is used to store any type of data
+anyName = 10;
+anyName = "Hello";
+anyName = true;
+anyName = { name: "John" };
+anyName = [1, 2, 3];
