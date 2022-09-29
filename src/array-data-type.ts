fe6f3ddbafe479054a90cf47eb8aafa95ae65651
rifatsaown <rifatsaown0@gmail.com>
2022-09-29 22:07:20 +0600

    array data type

---
+++ src/array-data-type.ts
@@ -0,0 +1,16 @@
+let userNames: string[]; // array data type
+userNames = ["John", "Jane", "Jack"];
+console.log(userNames[0]);
+
+userNames.push("Jill"); // add new element to array at the end
+console.log(userNames);
+
+userNames.pop(); // remove last element from array
+console.log(userNames);
+console.log(userNames.pop()); // remove last element from array and return it
+
+userNames.shift(); // remove first element from array
+console.log(userNames);
+
+userNames.unshift("Jill"); // add new element to array at the beginning
+console.log(userNames);
