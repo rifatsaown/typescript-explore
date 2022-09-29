c1b527854a923fe7f5fe23811dd053ae4d3c2879
rifatsaown <rifatsaown0@gmail.com>
2022-09-29 21:41:52 +0600

    data type

---
+++ dist/built-in-type.js
@@ -0,0 +1,13 @@
+"use strict";
+let rifat; // any data type
+let userID = 123; // number data type
+let firstName = "John"; // string data type
+let lastName = "Snow"; // string data type
+let isUser = true; // boolean data type
+let userName = firstName.concat(" ", lastName); // string data type
+console.log(`Id is:${userID} and name is:${userName} and is user:${isUser}`);
+console.log(userName.toUpperCase());
+console.log(userName.toLowerCase());
+console.table(userName.split(" "));
+console.log(userName.length);
+console.log(userID.toString());
---
+++ src/built-in-type.ts
@@ -0,0 +1,14 @@
+let rifat; // any data type
+let userID:number = 123; // number data type
+let firstName:string = "John"; // string data type
+let lastName:string = "Snow"; // string data type
+let isUser:boolean = true; // boolean data type
+let userName:string = firstName.concat(" ",lastName); // string data type
+
+console.log(`Id is:${userID} and name is:${userName} and is user:${isUser}`);
+
+console.log(userName.toUpperCase());
+console.log(userName.toLowerCase());
+console.table(userName.split(" "));
+console.log(userName.length);
+console.log(userID.toString());
